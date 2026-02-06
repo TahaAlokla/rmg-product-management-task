9c222a9ba40c8f609a07d955c3ef268294dc3345
unknown <taha.aloklaa1996@gmail.com>
2026-02-06 20:34:24 +0300

    update products list logic

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,12 +6,14 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideTransloco } from '@jsverse/transloco';
 import { TranslocoHttpLoader } from './core/transloco-loader';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideHttpClient(),
     provideTransloco({
       config: {
         availableLangs: ['en', 'ar'],
--- src/app/features/products/pages/products-list/products-list.ts
+++ src/app/features/products/pages/products-list/products-list.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { DecimalPipe, JsonPipe, NgFor, NgIf, NgOptimizedImage } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { ProductService } from '../../services/product.service';
+import type { Product } from '../../models/product';
+import { ProductDialog } from '../../components/product-dialog/product-dialog';
+import { TranslocoModule } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-products-list',
-  imports: [],
+  imports: [
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    NgOptimizedImage,
+    DecimalPipe,
+    TranslocoModule
+  ],
   templateUrl: './products-list.html',
   styleUrl: './products-list.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductsList {
+  protected readonly productService = inject(ProductService);
+  private _dialog = inject(MatDialog);
 
+  openAdd(): void {
+    const ref = this._dialog.open(ProductDialog, {
+      data: {},
+      width: '520px'
+    });
+    ref.afterClosed().subscribe((value: Omit<Product, 'id'> | undefined) => {
+      if (value) {
+        this.productService.add(value);
+      }
+    });
+  }
+
+  openEdit(p: Product): void {
+    const ref = this._dialog.open(ProductDialog, {
+      data: { product: p },
+      width: '520px'
+    });
+    ref.afterClosed().subscribe((value: Omit<Product, 'id'> | undefined) => {
+      if (value) {
+        this.productService.update(p.id, value);
+      }
+    });
+  }
+
+  deleteProduct(id: number): void {
+    this.productService.remove(id);
+  }
 }
