6711c8d2dfef2ae2c40a812a912cab7001e1c1a2
unknown <taha.aloklaa1996@gmail.com>
2026-02-06 04:24:25 +0300

    update

--- src/app/features/home/pages/dashboard/dashboard.ts
+++ src/app/features/home/pages/dashboard/dashboard.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { TranslocoModule } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterLink,
+    TranslocoModule
+  ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
--- src/app/layouts/dashboard-layout-page/dashboard-layout-page.ts
+++ src/app/layouts/dashboard-layout-page/dashboard-layout-page.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { TranslocoModule } from '@jsverse/transloco';
+import { AuthService } from '../../core/auth/services/auth-service';
+import { ThemeToggleComponent } from '../../shared/components/theme-toggle/theme-toggle.component';
 
 @Component({
   selector: 'app-dashboard-layout-page',
-  imports: [],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatIconModule
+    ,
+  ],
   templateUrl: './dashboard-layout-page.html',
   styleUrl: './dashboard-layout-page.css',
 })
 export class DashboardLayoutPage {
-
 }
