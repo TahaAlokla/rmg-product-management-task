3d345e44fde965b0fd8486e80d756c169843e3b2
unknown <taha.aloklaa1996@gmail.com>
2026-02-07 03:16:18 +0300

    update readem file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,16 @@
 import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-
 import { routes } from './app.routes';
-
 import { provideTransloco } from '@jsverse/transloco';
 import { TranslocoHttpLoader } from './core/transloco-loader';
 import { provideHttpClient } from '@angular/common/http';
 import { MatPaginatorIntl } from '@angular/material/paginator';
 import { TranslocoPaginatorIntlService } from './core/services/transloco-paginator-intl.service';
-
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
+
     provideHttpClient(),
 
     provideTransloco({
